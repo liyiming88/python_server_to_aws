9c056202689c083393456e7197afcd29404bba5f
YimingLi <574651401@qq.com>
2023-11-17 19:40:26 +0800

    update

--- server.py
+++ server.py
@@ -1,40 +1,45 @@
 import flask
-import logging
 from flask import jsonify, request
 
 app = flask.Flask(__name__)
-app.config['DEBUG'] = True
-logging.basicConfig(filename='app.log', level=logging.INFO)
+
+data = {}
+previous_data = {'id':'previous_default'}
 
 @app.route('/', methods=['GET'])
 def hello():
-    response = [{"message": "Hello, World ma!"}]
+    if 'id' in data and data['id']:
+        if previous_data['id'] != data['id']:
+            response = {
+                'sender': data.get('sender'),
+                'subject': data.get('subject'),
+                'content': data.get('content'),
+                'id': data.get('id'),
+                'new_data': True
+            }
+            previous_data['id'] = data['id']
+        else:
+            response = {
+            'new_data': False
+        }
+    else:
+        response = {
+            'new_data': False
+        }
     return jsonify(response)
 
 @app.route('/postdata', methods=['POST'])
 def post_data():
+    global data
     data = request.get_json()
-    sender = data.get('sender')
-    subject = data.get('subject')
-    content = data.get('content')
-    id = data.get('id')
-    
-    # 在这里对接收到的数据进行处理，可以根据需求自定义逻辑
     
     response = {
-        "sender": sender,
-        "subject": subject,
-        "content": content,
-        "id": id
+        'sender': data.get('sender'),
+        'subject': data.get('subject'),
+        'content': data.get('content'),
+        'id': data.get('id')
     }
-
-    logger = logging.getLogger('postdata')
-    logger.info("Received data:")
-    logger.info("Sender: %s", sender)
-    logger.info("Subject: %s", subject)
-    logger.info("Content: %s", content)
-    logger.info("ID: %s", id)
-
+    
     return jsonify(response)
 
 if __name__ == '__main__':
