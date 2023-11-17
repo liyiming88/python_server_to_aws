346171dbd39dc11fc4740406c396d6b413989b19
YimingLi <574651401@qq.com>
2023-11-17 14:14:24 +0800

    response json

--- zapier.py
+++ zapier.py
@@ -1,10 +1,12 @@
 import flask
+from flask import jsonify
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    response = {"message": "Hello, World!"}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
