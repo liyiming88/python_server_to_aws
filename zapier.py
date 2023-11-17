029a672fd01a08f370b76a7b1290e4beb0a7dfd0
YimingLi <574651401@qq.com>
2023-11-17 13:16:33 +0800

    aa

--- zapier.py
+++ zapier.py
@@ -1,22 +1,10 @@
-import http.server
-import socketserver
+import flask
 
-# 创建处理GET请求的处理器
-class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'Hello, World!')
+app = flask.Flask(__name__)
 
-# 定义服务器的地址
-host = 'localhost'
-port = 3000
-address = (host, port)
+@app.route('/')
+def hello():
+    return "Hello, World!"
 
-# 创建服务器对象
-server = socketserver.TCPServer(address, MyRequestHandler)
-
-# 启动服务器
-print(f'Starting server on http://{host}:{port}')
-server.serve_forever()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
