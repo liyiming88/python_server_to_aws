dd149aa64e88bc1a3d59c39fa28cc2e5a0f3d10e
YimingLi <574651401@qq.com>
2023-11-17 17:53:49 +0800

    update

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 app = flask.Flask(__name__)
 
@@ -10,9 +10,25 @@ def hello():
 
 @app.route('/postdata', methods=['POST'])
 def post_data():
-    data = flask.request.json
-    # 在这里对接收到的数据进行处理
-    response = {"message": "Data received successfully"}
+    data = request.get_json()
+    sender = data.get('sender')
+    subject = data.get('subject')
+    content = data.get('content')
+    id = data.get('id')
+    
+    # 在这里对接收到的数据进行处理，可以根据需求自定义逻辑
+    
+    response = {
+        "sender": sender,
+        "subject": subject,
+        "content": content,
+        "id": id
+    }
+    print("Received data:")
+    print("Sender:", sender)
+    print("Subject:", subject)
+    print("Content:", content)
+    print("ID:", id)
 
     return jsonify(response)
 
