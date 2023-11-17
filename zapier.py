f8b4dcd7760d424b598f504b5d10ff1022a6ef14
YimingLi <574651401@qq.com>
2023-11-17 16:32:57 +0800

    update

--- zapier.py
+++ zapier.py
@@ -3,10 +3,19 @@ from flask import jsonify
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    response = {"message": "Hello, World!"}
-    return jsonify(response)
+    if flask.request.method == 'POST':
+        # 处理POST请求
+        data = flask.request.json
+        # 在这里对数据进行处理
+        response = {"message": " Post, Hello, World!"}
+
+        return jsonify(response)
+    else:
+        # 处理GET请求
+        response = [{"message": "Get, Hello, World!"}]
+        return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
